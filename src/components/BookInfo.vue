1525db191eaa2bf27b054632f36780a4e42cfc57
kosi <kosisochukwu.asuzu@e4email.net>
2022-09-22 18:42:33 +0100

    feat: final application setup

--- src/components/BookInfo.vue
+++ src/components/BookInfo.vue
@@ -14,16 +14,39 @@
     const route = useRoute()
     const book = ref()
     const isLoading = ref(true)
+    const input_book_name = ref()
+    const input_book_isbn =ref()
+
 
     const getBook = async () => {
   try {
     const { data } = await axios.get(`http://waje-smart-backend.herokuapp.com/api/books/` + route.params.id)
     book.value = data
+
+    input_book_isbn.value = data.isbn
+    input_book_name.value = data.name
+
     isLoading.value = false
   } catch (error) {
     console.log({error})
   }
 }
+
+  const updateBook = async () =>{
+    const data = {
+      "name": input_book_name.value,
+      "isbn": input_book_isbn.value
+    }
+    try{
+      const response = await axios.patch('http://waje-smart-backend.herokuapp.com/api/books/'+ route.params.id + "/", data)
+      console.log(response)
+      isLoading.value = true
+      getBook()
+      
+    }catch(error){
+      console.log({error})
+    }
+  }
 
 getBook();
     </script>
@@ -31,7 +54,7 @@ getBook();
     <template>
     <div v-if="isLoading">Getting data</div>
     <div v-else>
-      <div class="about">
+      <div>
       <WelcomeItem>
         <template #icon>
           <DocumentationIcon />
@@ -51,6 +74,19 @@ getBook();
         <h4>FIRST NAME : {{book.author.first_name}}</h4>
         <h4>LAST NAME: {{book.author.last_name}}</h4>
       </WelcomeItem>
+      <br/>
+      <h2>Update Book</h2>
+      <br/>
+      <form method="post" @submit.prevent="updateBook">
+        <h5>NAME</h5>
+        <input v-model="input_book_name" type="text" name="name" id="name">
+        <h5>ISBN</h5>
+        <input v-model="input_book_isbn" type="text" name="name" id="name">
+        <br/>
+        <button type="submit">
+          Update Book
+        </button>
+      </form>
     </div>
     </div>    
     </template>
