6ac6e03b417cefb342b568ba9768742ef80e8e2a
kosi <kosisochukwu.asuzu@e4email.net>
2022-09-22 16:07:13 +0100

    feat: setup book list and details page

--- src/components/BookInfo.vue
+++ src/components/BookInfo.vue
@@ -5,30 +5,53 @@
     import EcosystemIcon from './icons/IconEcosystem.vue'
     import CommunityIcon from './icons/IconCommunity.vue'
     import SupportIcon from './icons/IconSupport.vue'
+    import axios from 'axios';
+
+
+    import { useRoute } from 'vue-router'
+    import { ref, watch } from 'vue'
+
+    const route = useRoute()
+    const book = ref()
+    const isLoading = ref(true)
+
+    const getBook = async () => {
+  try {
+    const { data } = await axios.get(`http://waje-smart-backend.herokuapp.com/api/books/` + route.params.id)
+    book.value = data
+    isLoading.value = false
+  } catch (error) {
+    console.log({error})
+  }
+}
+
+getBook();
     </script>
     
     <template>
+    <div v-if="isLoading">Getting data</div>
+    <div v-else>
+      <div class="about">
       <WelcomeItem>
         <template #icon>
           <DocumentationIcon />
         </template>
         <template #heading>Book Info</template>
-    
-        Click
-        <a href="https://vuejs.org/" target="_blank" rel="noopener">here</a>
-        to get more information about the book
+        <h4>NAME : {{book.name}}</h4>
+        <h4>ISBN: {{book.isbn}}</h4>
+        <h4>CREATED AT: {{book.created_at}}</h4>
+        <h4>UPDATED AT: {{book.updated_at}}</h4>
       </WelcomeItem>
     
       <WelcomeItem>
         <template #icon>
-          <DocumentationIcon />
+          <CommunityIcon />
         </template>
         <template #heading>Author</template>
-    
-        Click
-        <a href="https://vuejs.org/" target="_blank" rel="noopener">here</a>
-        to get more information about the book
+        <h4>FIRST NAME : {{book.author.first_name}}</h4>
+        <h4>LAST NAME: {{book.author.last_name}}</h4>
       </WelcomeItem>
-    
+    </div>
+    </div>    
     </template>
     
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,33 +1,47 @@
 <script setup>
+import axios from 'axios';
+import moment from 'moment'
+
 import WelcomeItem from './WelcomeItem.vue'
 import DocumentationIcon from './icons/IconDocumentation.vue'
 import ToolingIcon from './icons/IconTooling.vue'
 import EcosystemIcon from './icons/IconEcosystem.vue'
 import CommunityIcon from './icons/IconCommunity.vue'
 import SupportIcon from './icons/IconSupport.vue'
+import { ref } from 'vue'
+
+const books = ref([])
+
+const getBooks = async () => {
+  try {
+    const { data } = await axios.get(`http://waje-smart-backend.herokuapp.com/api/books/`)
+    console.log('data', data);
+    books.value = data
+  } catch (error) {
+    console.log({error})
+  }
+}
+
+getBooks();
+
+
 </script>
 
 <template>
-  <WelcomeItem>
+  
+  <WelcomeItem v-for="book in books">
     <template #icon>
       <DocumentationIcon />
     </template>
-    <template #heading>Documentation</template>
+    <template #heading>{{book.name}} by {{book.author.first_name}} {{book.author.last_name}}</template>
 
-    Click
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">here</a>
-    to get more information about the book
-  </WelcomeItem>
+    Click 
+    <RouterLink :to="`/book/${book.id}`"> 
+    here
+    </RouterLink>
 
-  <WelcomeItem>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>Documentation</template>
-
-    Click
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">here</a>
-    to get more information about the book
+    to get more information about the book 
+    <h6>{{book.created_at}}</h6>
   </WelcomeItem>
 
 </template>
